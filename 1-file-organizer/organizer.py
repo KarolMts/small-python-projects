99254ac19ca45d259e12af5dcca6cab0306ea24c
Karol Matuszewski <krlmatuszewski1983@gmail.com>
2026-07-16 23:13:49 +0200

    Working file organizer: group and move files by extension

--- 1-file-organizer/organizer.py
+++ 1-file-organizer/organizer.py
@@ -3,15 +3,19 @@ from pathlib import Path
 def get_category(path):
     if path.suffix != '':
         return path.suffix
-    return 'inne'
+    return '.OTHERS'
 
 
-folder = Path(r'C:\Users\krlma\Downloads')
+folder = Path(r'C:\Users\krlma\test_folder')
 
 d = {}
 for  element in folder.iterdir():
     if element.is_dir():
         continue
-    d.setdefault(get_category(element), []).append(element.name)
+    d.setdefault(get_category(element), []).append(Path(element))
     
-print(d)
+for category, files in d.items():
+    target_dir = folder / category[1:]
+    target_dir.mkdir(exist_ok=True)
+    for path in files:
+        path.rename(target_dir / path.name) 
