3da6b64361674df3fb6cf4d0279bf7e042fbaa83
Karol Matuszewski <krlmatuszewski1983@gmail.com>
2026-07-16 21:23:18 +0200

    Group files by extensions into a dictionary

--- 1-file-organizer/organizer.py
+++ 1-file-organizer/organizer.py
@@ -1,7 +1,17 @@
 from pathlib import Path
 
+def get_category(path):
+    if path.suffix != '':
+        return path.suffix
+    return 'inne'
+
 
 folder = Path(r'C:\Users\krlma\Downloads')
-for  path in folder.iterdir():
-    if path.is_file():
-        print(path)
+
+d = {}
+for  element in folder.iterdir():
+    if element.is_dir():
+        continue
+    d.setdefault(get_category(element), []).append(element.name)
+    
+print(d)
