f0a30e3aefc52bdf347696a4fc0f8145038d14e6
Karol Matuszewski <krlmatuszewski1983@gmail.com>
2026-08-03 23:43:02 +0200

    Improve invoice layout: dates, payments, etc.

--- 3-invoice-generator/pdf_maker.py
+++ 3-invoice-generator/pdf_maker.py
@@ -26,16 +26,23 @@ def create_pdf(data, charges, total):
     pdf = InvoicePDF(data)
     pdf.add_page()
 
-    vendor = data["vendor"]
-    pdf.multi_cell(0, 10, text=f"Sprzedawca: {vendor['name']} {vendor['address']} {vendor['tax_id']}", align="L",border=1, new_x="LMARGIN", new_y="NEXT")
-    pdf.ln(5)
+    pdf.cell(0, 8, text=f"Data wystawienia: {data['issue_date']}", align="L", new_x="LMARGIN", new_y="NEXT")
+    pdf.ln(4)
+    pdf.cell(0, 8, text=f"Termin płatności: {data['due_date']}", align="L", new_x="LMARGIN", new_y="NEXT")
+    pdf.ln(10)
 
+    pdf.set_font("DejaVu", size=12)
+    y = pdf.get_y()
+    x_left = pdf.get_x()
+    vendor = data["vendor"]
+    pdf.multi_cell(85, 10, text=f"Sprzedawca: {vendor['name']}\n Adres: {vendor['address']}\n NIP:{vendor['tax_id']}", align="L",border=1, new_x="LMARGIN", new_y="NEXT")
+    pdf.set_xy(x_left + 85 +20, y)
     customer = data["customer"]
-    pdf.multi_cell(0, 10, text=f"Nabywca: {customer['name']} {customer['address']}", align="L", border=1, new_x="LMARGIN", new_y="NEXT")
-    pdf.ln(5)
+    pdf.multi_cell(0, 10, text=f"Nabywca: {customer['name']}\n Adres: {customer['address']}", align="L", border=1, new_x="LMARGIN", new_y="NEXT")
+    pdf.ln(40)
 
     pdf.set_font("DejaVu", size=8)
-    pdf.cell(w=35, h=5, text="Nazwa", align="C", border=1)
+    pdf.cell(w=38, h=5, text="Nazwa", align="C", border=1)
     pdf.cell(w=25, h=5, text="Ilość", align="C", border=1)
     pdf.cell(w=25, h=5, text="Cena netto", align="C", border=1)
     pdf.cell(w=25, h=5, text="Stawka VAT", align="C", border=1)
@@ -44,21 +51,31 @@ def create_pdf(data, charges, total):
     pdf.cell(w=25, h=5, text="Wartość brutto", align="C", border=1, new_x="LMARGIN", new_y="NEXT")
 
     for item, charge in zip(data['line_items'], charges):
-        pdf.cell(w=35, h=5, text=f"{item['description']}", align="C", border=1)
+        pdf.cell(w=38, h=5, text=f"{item['description']}", align="C", border=1)
         pdf.cell(w=25, h=5, text=f"{item['quantity']}", align="C", border=1)
-        pdf.cell(w=25, h=5, text=f"{item['unit_price']}", align="C", border=1)
-        pdf.cell(w=25, h=5, text=f"{item['tax_rate']}", align="C", border=1)
-        pdf.cell(w=25, h=5, text=f"{charge.net}", align="C", border=1)
-        pdf.cell(w=25, h=5, text=f"{charge.vat}", align="C", border=1)
-        pdf.cell(w=25, h=5, text=f"{charge.gross}", align="C", border=1, new_x="LMARGIN", new_y="NEXT")
+        pdf.cell(w=25, h=5, text=f"{float(item['unit_price']):>,.2f}", align="C", border=1)
+        pdf.cell(w=25, h=5, text=f"{float(item['tax_rate'])*100}%", align="C", border=1)
+        pdf.cell(w=25, h=5, text=f"{float(charge.net):>,.2f}", align="C", border=1)
+        pdf.cell(w=25, h=5, text=f"{float(charge.vat):>,.2f}", align="C", border=1)
+        pdf.cell(w=25, h=5, text=f"{float(charge.gross):>,.2f}", align="C", border=1, new_x="LMARGIN", new_y="NEXT")
 
-    pdf.ln(2)
+    pdf.ln(5)
     pdf.set_font("DejaVu", size=12)
     pdf.cell(w=110, h=5, text="Razem:", align="R")
     pdf.cell(w=25, h=5, text=f"{total.net}", align="C", border=1)
     pdf.cell(w=25, h=5, text=f"{total.vat}", align="C", border=1)
     pdf.cell(w=25, h=5, text=f"{total.gross}", align="C", border=1)
-    pdf.cell(w=15, h=5, text=f"{data['currency']}", align="C", border=1)
+    pdf.cell(w=15, h=5, text=f"{data['currency']}", align="C", border=1, new_x="LMARGIN", new_y="NEXT")
+    pdf.ln(10)
+
+    pay = data["payment"]
+    pdf.set_font("DejaVu", size=14)
+    pdf.cell(w=25, h=5, text=f"Płaność:", align="L", new_x="LMARGIN", new_y="NEXT")
+    pdf.ln(3)
+    pdf.set_font("DejaVu", size=10)
+
+    pdf.set_x(pdf.get_x() + 5)
+    pdf.multi_cell(w=0, h=6, text=f"Bank:  {pay['bank']}\nNumer konta:  {pay['account_number']}\nSposób:  {pay['form']}\nTytułem:  {pay['title']}", align="L")
 
     pdf.output("faktura.pdf")
 
